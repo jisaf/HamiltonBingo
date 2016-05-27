2560a239ba906f750fac5bbda478f32eb4234258
Justin Isaf <code@justinisaf.com>
2016-05-27 08:48:47 -0400

    FB signup and invite friends

--- browser/js/fsa/home.controller.js
+++ browser/js/fsa/home.controller.js
@@ -2,4 +2,6 @@ app.controller('HomeCtrl', function ($scope, BingoFactory) {
    $scope.greeting = 'Hi Justin';
    $scope.rows = [['My','Name','Is','Jason','Unger'],['All','Shall','Bow','Before','Me'],['How','Much','Wood','Could'],['A','Woodchuck','Chuck','If','A'],[]]
    console.log(BingoFactory.generate())
+
+
 });
